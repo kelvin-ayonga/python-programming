bfd90512011bd638068210b768795ddf3f77d5bf
Isaiah kelvin Ayonga <kelvin.ayonga@pixeledge.io>
2025-02-21 13:18:30 +0300

    code: Add number manipulation file

---
+++ day 2/number_manipulation.py
@@ -0,0 +1,30 @@
+bmi = 84 / 1.65 ** 2
+
+# Flooring a number
+print(int(bmi))
+# Rounding  a number
+print(round(bmi))
+
+print(round(bmi, 2)) # 30.85
+
+
+#Assignment Operators
+score = 0
+
+score += 1
+print(score)
+
+score -= 1
+print(score)
+
+score *= 1
+print(score)
+
+score /= 1
+print(score)
+
+# f-strings
+
+age = 12
+
+print(f"I am {age} years old")
